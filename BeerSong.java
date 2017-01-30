badbeb224d6fcf5ec7a4abc829096b2465ce3450
Connor Kurschat <Connor Kurschat>
2017-01-30 14:24:41 -0500

    completed output loop and custom outputs for bottles 1 and 2

--- BeerSong.java
+++ BeerSong.java
@@ -6,18 +6,27 @@ package BeerSong;
  */
 public class BeerSong {
     
-    static int count = 10;
+    static int count = 99;
     
     public static void main(String[] args) {
         Ninety_Nine_Bottles_of_Beer();
     }
     
     public static void Ninety_Nine_Bottles_of_Beer() {
-        for (int i = count; i > 1 ; i-- ) {
+        for (int i = count; i > 2 ; i-- ) {
             System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer.");
             count -= 1; //SNEAKY
             System.out.println("take one down, pass it around, " + count + " bottles of beer on the wall."); 
         }
+        
+        //ALTERED OUTPUT THAT WILL RUN WHEN 99 THROUGH 3 ARE FINISHED LOOPING
+        System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer.");
+        count -= 1; //SNEAKY
+        System.out.println("take one down, pass it around, " + count + " bottle of beer on the wall.");
+        
+        //ONLY 1 BOTTLE LEFT, ALTERED ENDING
+        System.out.println(count + " bottle of beer on the wall, " + count + " bottle of beer.");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
     }
     
 }
