7c2e18236988c2d9f32af7ed15b0f3e5945fd90f
Connor Kurschat <Connor Kurschat>
2017-01-30 14:08:53 -0500

    adding basic foundation of functions, prints 99-2

--- BeerSong.java
+++ BeerSong.java
@@ -6,12 +6,18 @@ package BeerSong;
  */
 public class BeerSong {
     
+    static int count = 10;
+    
     public static void main(String[] args) {
-        
+        Ninety_Nine_Bottles_of_Beer();
     }
     
     public static void Ninety_Nine_Bottles_of_Beer() {
-        
+        for (int i = count; i > 1 ; i-- ) {
+            System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer.");
+            count -= 1; //SNEAKY
+            System.out.println("take one down, pass it around, " + count + " bottles of beer on the wall."); 
+        }
     }
     
 }
