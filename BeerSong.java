3c1acf30f01da71d4590347fb53df53d376ec3a4
Connor Kurschat <Connor Kurschat>
2017-01-30 14:03:47 -0500

    adding file with empty methods

--- BeerSong.java
+++ BeerSong.java
@@ -6,18 +6,11 @@ package BeerSong;
  */
 public class BeerSong {
     
-    static int count = 10;
-    
     public static void main(String[] args) {
         
-//        System.out.println("print number" + count); //PERFECT
-        
-        for (int i = count; i > 1 ; i-- ) {
-            System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer.");
-            count -= 1; //SNEAKY
-            System.out.println("take one down, pass it around, " + count + " bottles of beer on the wall."); 
-        }
-        
+    }
+    
+    public static void Ninety_Nine_Bottles_of_Beer() {
         
     }
     
